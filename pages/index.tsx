ee78203cf1d1454066a3c7ce660700fcbb681de0
dev global <pavankumar.s12459@gmail.com>
2022-06-25 17:26:25 +0530

    Added Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <title>Hulu</title>
       </Head>
       <Header />
+      <Nav/>
     </div>
   );
 };
